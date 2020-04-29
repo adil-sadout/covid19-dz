910d38f4110cf2a7eb949e3fbb412941f519b3ac
Adil Sadout <adelsadout@hotmail.com>
2020-04-29 03:28:32 +0100

    fixed api of algeria not working

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 /*API FOR COVID 19 DATA*/
 
 
-var api_url_global_stats = "https://coronavirus-monitor.p.rapidapi.com/coronavirus/worldstat.php"
-var api_url_algeria_stats = "https://coronavirus-monitor.p.rapidapi.com/coronavirus/latest_stat_by_country.php?country=algeria"
+var api_url_global_stats = "https://covid-19-data.p.rapidapi.com/totals?format=json"
+var api_url_algeria_stats = "https://covid-19-data.p.rapidapi.com/country?format=json&name=Algeria"
 var api_url_algeria_wilaya = "https://api.healthdz.com/covid19/now"
 
 
@@ -11,19 +11,16 @@ async function getCovidGlobalStats(){
 		const response = await fetch(api_url_global_stats, {
 		"method": "GET",
 		"headers": {
-			"x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+			"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
 			"x-rapidapi-key": "95164ec703msh88844cc1f0d8036p1ea4dfjsnf5e6e08f6b94"
 			}
 		})
 
 		const data =await response.json();
-		const{total_cases,total_deaths,total_recovered,statistic_taken_at} = data
-		
-
-		document.getElementById('number0').textContent = statistic_taken_at;
-		document.getElementById('number1').textContent = total_cases;
-		document.getElementById('number2').textContent = total_deaths;
-		document.getElementById('number3').textContent = total_recovered;
+		const{confirmed,deaths,recovered} = data[0];
+		document.getElementById('number1').textContent = confirmed;
+		document.getElementById('number2').textContent = deaths;
+		document.getElementById('number3').textContent = recovered;
 		
 
 }
@@ -33,17 +30,16 @@ async function getCovidAlgeriaStats(){
 		const response = await fetch(api_url_algeria_stats, {
 			"method": "GET",
 			"headers": {
-				"x-rapidapi-host": "coronavirus-monitor.p.rapidapi.com",
+				"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
 				"x-rapidapi-key": "95164ec703msh88844cc1f0d8036p1ea4dfjsnf5e6e08f6b94"
 			}
 		})
 
 		const data1 =await response.json();
-		const {latest_stat_by_country} = data1;
-		const data = latest_stat_by_country[0];
-		document.getElementById('number4').textContent = data.total_cases;
-		document.getElementById('number5').textContent = data.total_deaths;
-		document.getElementById('number6').textContent = data.total_recovered;
+		const {confirmed,recovered,deaths} = data1[0];
+		document.getElementById('number4').textContent = confirmed;
+		document.getElementById('number5').textContent = deaths;
+		document.getElementById('number6').textContent = recovered;
 		
 
 }
